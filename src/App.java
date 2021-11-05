f0c3c2582b3ebf91947b73dd54ea1a64ebee84ab
MartinSilen <iokiokna@gmail.com>
2021-11-05 12:47:58 +0200

    kek

---
+++ src/App.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        double sum = 0;
+        Scanner inp =  new Scanner(System.in);
+        while(true){
+            System.out.println("I will add up numbers you enter. Print \"exit\" to stop");
+            try {
+                String com = inp.nextLine();
+                if (com.equalsIgnoreCase("exit")) {
+                    System.out.println("Goodbye");
+                    break;
+                }
+                double operand = Double.parseDouble(com);
+                sum += operand;} catch (Exception e){System.out.println("This is not a number!"); continue;}
+            System.out.println("Current sum is: " + sum);
+
+        }
+
+
+        }
+    }
